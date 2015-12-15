353806d19892919a478ee437ea1fc1e5423a5a5e
David Madl <git@abanbytes.eu>
2015-12-15 16:05:36 +0000

    get_git_revision()

--- scripts/build-moses.sh
+++ scripts/build-moses.sh
@@ -4,6 +4,8 @@ set -e
 #BUILD_OPTIONS=""
 BUILD_TYPE="Release"
 
+TMP=/tmp/build-moses.$$
+
 # later, arg
 AUTO_TARGET_DIR=/home/david/build/auto
 
@@ -50,13 +52,52 @@ shift $((OPTIND-1))
 [ "$1" = "--" ] && shift
 
 
+OPT_DIR=$AUTO_TARGET_DIR/opt
+MOSES_CACHED_DIR=$AUTO_TARGET_DIR/cached-moses
 
-# pattern for directory name: moses.branch.rev.BuildType
-# this ensures we build different configs separately
-MOSES_TARGET_DIR=$AUTO_TARGET_DIR/moses.$MOSES_BRANCH.$MOSES_REV.$BUILD_TYPE
 
-OPT_DIR=$MOSES_TARGET_DIR/../opt
-MOSES_CACHED_DIR=$MOSES_TARGET_DIR/../cached-moses
+
+
+function ensure_have_moses_cached() {
+  if [ ! -e $MOSES_CACHED_DIR ]; then
+      # maintain a recent moses git repo to avoid always having to pull the History of Time.
+      git clone $MOSES_CACHED_REPO $MOSES_CACHED_DIR
+  fi
+}
+
+function ensure_have_dependencies() {
+  if [ ! -e $OPT_DIR ]; then
+      # set up opt tools ...
+
+      mkdir -p $OPT_DIR
+
+      pushd $(dirname $OPT_DIR)
+      # build stuff in ./build, install in ./opt
+      make -f $(dirname $0)/install-dependencies.gmake
+      popd
+  fi
+}
+
+function get_git_revision() {
+    ensure_have_moses_cached
+    mkdir -p $TMP
+    git clone $MOSES_CACHED_DIR $TMP/moses.check
+    pushd $TMP/moses.check
+
+    git remote remove origin
+    git remote add origin $MOSES_SRC_REPO
+    git fetch
+    git checkout $MOSES_BRANCH
+    git checkout $MOSES_REV
+
+    # first 7 characters of revision
+    MOSES_REV=$(git log | awk '/^commit/ { print(substr($2, 0, 7)); exit; }')
+
+    # didn't want to use "git describe --dirty" here, because it's so long.
+
+    popd
+    rm -rf $TMP
+}
 
 
 # backup stdout to fd=6
@@ -65,31 +106,29 @@ exec 6<&1
 exec 1>&2
 
 
+#if [ "$MOSES_REV" == "$MOSES_BRANCH" -o "$MOSES_REV" == "HEAD" ]; then
+#    # need to get the actual revision!
+#    #MOSES_REV=$(get_git_revision)
+#    get_git_revision
+#fi
+
+# always get git revision, this way we cannot fail to get it right.
+get_git_revision
+
+
+# pattern for directory name: moses.branch.rev.BuildType
+# this ensures we build different configs separately
+MOSES_TARGET_DIR=$AUTO_TARGET_DIR/moses.$MOSES_BRANCH.$MOSES_REV.$BUILD_TYPE
 mkdir -p $MOSES_TARGET_DIR
 
-if [ ! -e $OPT_DIR ]; then
-    # set up opt tools ...
-    
-    mkdir -p $OPT_DIR
-    
-    pushd $(dirname $OPT_DIR)
-    # build stuff in ./build, install in ./opt
-    make -f $(dirname $0)/install-dependencies.gmake
-    popd
-fi
 
 if [ -e $MOSES_TARGET_DIR/bin/moses ]; then
     # there is already a build for this revision
     exit 0
 fi
 
-if [ ! -e $MOSES_CACHED_DIR ]; then
-    # maintain a recent moses git repo to avoid always having to pull the History of Time.
-    git clone $MOSES_CACHED_REPO $MOSES_CACHED_DIR
-fi
-
-# make sure we don't go via $MOSES_TARGET_DIR that's not going to exist
-MOSES_CACHED_DIR=$(readlink -f $MOSES_CACHED_DIR)
+ensure_have_dependencies
+ensure_have_moses_cached
 
 # clean up if there was something left over
 rm -rf "$MOSES_TARGET_DIR"
