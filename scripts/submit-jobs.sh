a7aa3fa9513057723b98d6625127ec8328280f3a
David Madl <git@abanbytes.eu>
2016-01-12 15:28:44 +0000

    submit-jobs.sh to cluster

---
+++ scripts/submit-jobs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+HOSTS="$1"
+shift
+
+pushd ~/mmt/run-bricks/mmt
+dir=$(date "+%Y-%m-%d_%H-%M-%S")
+echo $dir
+mkdir $dir || exit 1
+pushd $dir
+
+. /fs/lofn0/dmadl/mmt/run-bricks/env.sh
+
+for host in $HOSTS; do
+  if [ $# -eq 0 ]; then
+    echo >&2 "note: more hosts than jobs."
+    break
+  fi
+
+  experiment="$1"
+  shift
+
+  if [ ! -e /fs/lofn0/dmadl/mmt/bricks/examples/mmt/$experiment.cfg ]; then
+    echo >&2 "error: no mmt experiment $experiment.cfg exists."
+    continue
+  fi
+
+  # debug print job scheduling
+  echo "$experiment @ $host"
+
+  # set up folder
+  mkdir -p $experiment && pushd $experiment
+  target=$(pwd)
+  pushd /fs/lofn0/dmadl/mmt/bricks/examples/mmt
+  for part in *.cfg; do
+    ln -sf /fs/lofn0/dmadl/mmt/bricks/examples/mmt/$part $target/$part
+  done
+  popd
+  ln -sf /fs/lofn0/dmadl/mmt/bricks/examples/mmt/$experiment.cfg experiment.cfg
+  bricks.py experiment.cfg
+  popd
+
+  # submit job to host
+  ssh $host "
+    . /fs/lofn0/dmadl/mmt/run-bricks/env.sh
+    cd ~/mmt/run-bricks/mmt/$dir/$experiment
+    nice nohup redo >> nohup.out 2>&1 &
+    echo \$! > redo.pid
+  "
+done
+
+popd  # $dir
+popd  # run-bricks/mmt
+
+if [ $# -gt 0 ]; then
+  echo >&2 "warning: some jobs have not been assigned hosts:"
+  echo >&2 "$@"
+fi
