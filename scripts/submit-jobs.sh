ece99cd9da9ddaa783b11066f4a61cc2ea3daada
David Madl <git@abanbytes.eu>
2016-01-12 15:30:34 +0000

    pushd/popd  >/dev/null

--- scripts/submit-jobs.sh
+++ scripts/submit-jobs.sh
@@ -3,11 +3,11 @@
 HOSTS="$1"
 shift
 
-pushd ~/mmt/run-bricks/mmt
+pushd ~/mmt/run-bricks/mmt >/dev/null
 dir=$(date "+%Y-%m-%d_%H-%M-%S")
 echo $dir
 mkdir $dir || exit 1
-pushd $dir
+pushd $dir >/dev/null
 
 . /fs/lofn0/dmadl/mmt/run-bricks/env.sh
 
@@ -29,16 +29,16 @@ for host in $HOSTS; do
   echo "$experiment @ $host"
 
   # set up folder
-  mkdir -p $experiment && pushd $experiment
+  mkdir -p $experiment && pushd $experiment >/dev/null
   target=$(pwd)
-  pushd /fs/lofn0/dmadl/mmt/bricks/examples/mmt
+  pushd /fs/lofn0/dmadl/mmt/bricks/examples/mmt >/dev/null
   for part in *.cfg; do
     ln -sf /fs/lofn0/dmadl/mmt/bricks/examples/mmt/$part $target/$part
   done
-  popd
+  popd >/dev/null
   ln -sf /fs/lofn0/dmadl/mmt/bricks/examples/mmt/$experiment.cfg experiment.cfg
   bricks.py experiment.cfg
-  popd
+  popd >/dev/null
 
   # submit job to host
   ssh $host "
@@ -49,8 +49,8 @@ for host in $HOSTS; do
   "
 done
 
-popd  # $dir
-popd  # run-bricks/mmt
+popd >/dev/null  # $dir
+popd >/dev/null  # run-bricks/mmt
 
 if [ $# -gt 0 ]; then
   echo >&2 "warning: some jobs have not been assigned hosts:"
