0423744157cff5f7591bfe647ab9ecc3a372dea0
David Madl <git@abanbytes.eu>
2016-01-22 13:00:45 +0000

    change to MMT paths

--- scripts/testing/build-moses.sh
+++ scripts/testing/build-moses.sh
@@ -3,14 +3,19 @@
 branch=master
 rev=HEAD
 
-BUILD_MOSES=/home/dmadl/mmt/bricks/scripts/build-moses.sh
-BUILD_MOSES_OPTS="-b $branch -a /fs/saxnot0/dmadl/build/auto -g /home/dmadl/gcc-4.9.2"
+TEST_FRAMEWORK=/home/shared/mmt/testing
+
+BUILD_MOSES=$TEST_FRAMEWORK/bricks/scripts/build-moses.sh
+BUILD_MOSES_OPTS="-b $branch -a $TEST_FRAMEWORK/build -g /home/dmadl/gcc-4.9.2"
 
 # obtain the given / most recent revision number of the branch
 gitrev=$($BUILD_MOSES $BUILD_MOSES_OPTS -r $rev -v)
 
 # build the most recent revision in the staging directory
 moses=$($BUILD_MOSES $BUILD_MOSES_OPTS -r $gitrev)
+rv=$?
 
 echo gitrev=$gitrev
 echo moses=$moses
+
+exit $rv
