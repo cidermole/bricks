3cdffa5c0986115e6fd95fd5c0912ba3f5e1c7af
David Madl <git@abanbytes.eu>
2016-01-25 11:51:19 +0000

    fixup rename option

--- scripts/moses-ini-copy-setup.py
+++ scripts/moses-ini-copy-setup.py
@@ -20,7 +20,7 @@ def parseArguments():
     parser.add_argument('-f', '--input', dest='source_moses_ini', help='moses.ini in its original environment', nargs='?', default='/dev/stdin')
     parser.add_argument('-o', '--output', dest='target_moses_ini', help='target path to moses.ini or directory to store moses.ini', nargs='?', default='/dev/stdout')
     parser.add_argument('output_data_path', help='target path to a directory to store data files')
-    parser.add_argument('-n', '--no-overwrite', dest='noOverwrite', help='do not overwrite data files if they already exist', action='store_true')
+    parser.add_argument('-n', '--no-overwrite-data', dest='noOverwrite', help='do not overwrite data files if they already exist', action='store_true')
     parser.add_argument('-d', '--dry-run', dest='dryRun', help='do not actually copy data files, just print summary', action='store_true')
 
     args = parser.parse_args()
