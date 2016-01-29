e861ac56f50a000c824ce49823b0f87fcfb917f6
David Madl <git@abanbytes.eu>
2016-01-29 20:27:11 +0000

    run-tests.sh extended

--- run-tests.sh
+++ run-tests.sh
@@ -4,3 +4,5 @@
 
 python2 -m unittest "$@" tests.ConfigOverrideMapping
 python2 -m unittest "$@" tests.ConfigMiniPhraseTable
+python2 -m unittest "$@" tests.ConfigOutputList
+python2 -m unittest "$@" tests.TestEP
