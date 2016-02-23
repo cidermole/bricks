79864b208e8011dfec0e4bb4faa242fcc406faeb
David Madl <git@abanbytes.eu>
2016-02-23 12:38:42 +0100

    add build-pt.sh

---
+++ scripts/build-pt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Usage:
+#
+# export PATH=$MOSES_HOME/bin:$PATH
+# build-pt.sh crp.fr crp.en align prefix  # prefix="/path/to/pt/model."
+
+crp1="$1"
+crp2="$2"
+align="$3"
+prefix="$4"
+
+lang1="${crp1##*.}"
+lang2="${crp2##*.}"
+
+
+mtt-build -i -o "${prefix}.$lang1" < "$crp1" &
+proc1=$!
+mtt-build -i -o "${prefix}.$lang2" < "$crp2" &
+proc2=$!
+symal2mam "${prefix}.${lang1}-${lang2}.mam" < "$align" &
+proc3=$!
+wait $proc1 $proc2 $proc3
+
+mmlex-build "${prefix}" $lang1 $lang2 -o "${prefix}.${lang1}-${lang2}.lex"
+
