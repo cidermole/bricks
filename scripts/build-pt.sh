442bd34e14609815326004d5d19257dc7c0da9d4
David Madl <git@abanbytes.eu>
2016-02-25 12:21:03 +0100

    built-pt.sh: may pass additional args after $4, e.g. -m model.dmp (document map)

--- scripts/build-pt.sh
+++ scripts/build-pt.sh
@@ -4,19 +4,22 @@
 #
 # export PATH=$MOSES_HOME/bin:$PATH
 # build-pt.sh crp.fr crp.en align prefix  # prefix="/path/to/pt/model"
+#
+# may pass additional args after $4, e.g. -m model.dmp (document map)
 
 crp1="$1"
 crp2="$2"
 align="$3"
 prefix="$4"
+shift; shift; shift; shift
 
 lang1="${crp1##*.}"
 lang2="${crp2##*.}"
 
 
-mtt-build -i -o "${prefix}.$lang1" < "$crp1" &
+mtt-build -i -o "${prefix}.$lang1" "$@" < "$crp1" &
 proc1=$!
-mtt-build -i -o "${prefix}.$lang2" < "$crp2" &
+mtt-build -i -o "${prefix}.$lang2" "$@" < "$crp2" &
 proc2=$!
 symal2mam "${prefix}.${lang1}-${lang2}.mam" < "$align" &
 proc3=$!
